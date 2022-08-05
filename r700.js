e3b0fee58dc1bdcfc53baba0cd54ac381d94631b
SebastianOrtega <sortega@moviltrack.com>
2022-08-05 16:56:45 -0500

    agregado conversion a ascii y solo vista de numero

--- r700.js
+++ r700.js
@@ -2,8 +2,21 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+function hex_to_ascii(str1) {
+  var hex = str1.toString();
+  var str = "";
+  for (var n = 0; n < hex.length; n += 2) {
+    str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+  }
+  return str;
+}
+
 app.post("/", function (req, res) {
-  console.log("----------------------------\n", req.body);
+  //console.log("----------------------------\n", req.body);
+
+  req.body.map((tag) => {
+    console.log(hex_to_ascii(tag.tagInventoryEvent.epcHex).substring(8));
+  });
   res.end();
 });
 
